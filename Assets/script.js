bda3d36518ba48f06a7c1c7a2f1ad6082adc1eca
jwm <jwm3926@gmail.com>
2022-10-11 19:14:12 -0400

    added variable for url reddit

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,23 @@
-function getApi(){
-    var requestUrl = 'https://www.reddit.com/subreddits/search.json?q=hulk'
+// function getApi(){
+//     var requestUrl = 'https://www.reddit.com/subreddits/search.json?q=hulk'
 
-    fetch(requestUrl)
-        .then(function(response){
-            // console.log(data)
-            return response.json();
-        })
-}
-getApi()
+//     fetch(requestUrl)
+//         .then(function(response){
+//             // console.log(data)
+//             return response.json();
+//         })
+// }
+// getApi()
+
+var heroReddit = 'http://www.reddit.com/search.json?q={hero-name}'
+
+fetch(heroReddit)
+  .then(function(res) {
+    return res.json();   // Convert the data into JSON
+  })
+  .then(function(data) {
+    console.log(data);   // Logs the data to the console
+  })
+  .catch(function(err) {
+    console.log(err);   // Log error if any
+  });
