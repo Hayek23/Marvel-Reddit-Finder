83ab55b06ece4e984aece18fe35b21ec306d7ca9
jwm <jwm3926@gmail.com>
2022-10-11 19:54:44 -0400

    made some functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,24 @@
 var marvelApiKey = "caf809fd4bc0067858336835423deb52";
+var heroRedditURL = 'http://www.reddit.com/search.json?q={hero-name}';
+var hero = document.getElementById('hero');
+var subredditText = document.getElementById('fill-in');
+var btn = document.getElementById('submitbtn');
 
-function getApi(){
-    var requestUrl = 'https://www.reddit.com/subreddits/search.json?q=hulk'
+function setURL(){
+    var heroRedditURL = 'http://www.reddit.com/search.json?q=' + hero.value;
+}
 
-var heroReddit = 'http://www.reddit.com/search.json?q={hero-name}'
+console.log
 
-fetch(heroReddit)
+btn.addEventListener('click', showText);
+
+function showText(){
+    subredditText.textContent = hero.value
+};
+
+console.log(hero.value);
+
+fetch(heroRedditURL)
   .then(function(res) {
     return res.json();   // Convert the data into JSON
   })
@@ -15,5 +28,7 @@ fetch(heroReddit)
   .catch(function(err) {
     console.log(err);   // Log error if any
   });
+
+
 
 
